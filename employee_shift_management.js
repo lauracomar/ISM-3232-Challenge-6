f268811892692a3e682ab2ba27ea34fd13cc590f
Laura Comar <lautmcomar@gmail.com>
2024-09-22 16:06:50 -0400

    Create assignShift function

--- employee_shift_management.js
+++ employee_shift_management.js
@@ -13,4 +13,20 @@ function displayEmployeeShifts(employee) {
     employee.shifts.forEach(shifts => {
         console.log(`Shift Day: ${shifts.day}, Shift Time: ${shifts.hours}`) // find employees shift and displays date and time
     });
-}; 
+};
+
+// Task 3: Create a Function to Assign a New Shift
+function assignShift(name, day, hours) {
+    let employee = employees.find(empl => empl.name === name); // find employee in em ployees array (task 1)
+    if (!employee) {
+        console.log(`Employee not found`); // error message in case an employee is not found
+    };
+    let shiftAssigned = employee.shifts.some(shift => shift.day === day); // checking if employee has a shift on that day
+    if (shiftAssigned) {
+        console.log(`Employee already assigned to a shift on this day`); // display message if employee already has shift on that day 
+    }
+    else {
+        employee.shifts.push({ day: day, hours: hours });
+        console.log(`New shift assigned. Employee: ${name}, Day: ${day}, Time: ${hours}`); // display message confirming new shift has been assigned and display shift details
+    }; // if employee is available assign new shift to employee
+}
